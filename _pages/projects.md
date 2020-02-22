5655985f8837987c6fa7f99275c8edb11100ac96
KaustubhSathe <f2015466@pilani.bits-pilani.ac.in>
2020-02-23 04:41:28 +0530

    change

--- _pages/projects.md
+++ _pages/projects.md
@@ -2,4 +2,14 @@
 layout: archive
 permalink: /projects/
 author_profile: true
+classes: wide
 ---
+
+## Codeforces Problem Tracker Chrome Extension
+
+Created a chrome extension which keeps track of your understood but not yet implemented problems of codeforces and colors those problems as yellow.
+- Used chrome storage api to keep track of the problems.
+- Created a frontend using HTML + CSS to display the understood problems.
+- Got 14 upvotes and good feedback from the codeforces community regarding this extension.
+
+[Codeforces Blog](https://codeforces.com/blog/entry/73196)&nbsp;&nbsp;[Source Code](https://github.com/KaustubhSathe/Codeforces-Problem-Tracker)&nbsp;&nbsp;[Install Here](https://chrome.google.com/webstore/detail/codeforces-problem-tracke/mopegdapkaknnngnandcpimkoimcoioc?utm_source=chrome-ntp-icon&authuser=1)
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "Writing Jekyll Posts",
-        "excerpt":"sddddddddddddddddddd  ","categories": [],
+        "title": "Centroid Decomposition Explained",
+        "excerpt":"#include&lt;bits/stdc++.h&gt; #define pb push_back #define mp make_pair #define pii pair&lt;int,int&gt; using namespace std; int n; vector&lt;int&gt; adj[1000000+5]; vector&lt;int&gt; centroid[1000000+5]; bool is_centroid[1000000+5]; int sz[1000000+5],par[1000000+5],nn; vector&lt;pii&gt; disCentroidParent[100000+5]; void dfs(int u,int p = -1){ sz[u] = 1; for(auto x : adj[u]){ if(x == p || is_centroid[x])continue; dfs(x,u); sz[u] += sz[x]; } } void...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/writing-jekyll-posts/",
+        "url": "http://localhost:4000/centroid-decomposition-explained/",
         "teaser":null}]
