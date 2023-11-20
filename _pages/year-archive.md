b1922eafc6acd43a205f6e6939e47ef132cda88e
Kaustubh <ksathe@suki.ai>
2023-11-20 18:17:55 +0530

    chore: new site

--- _pages/year-archive.md
+++
@@ -1,6 +0,0 @@
----
-title: "Blog"
-permalink: /year-archive/
-layout: posts
-author_profile: true
----
--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,6 +0,0 @@
-var store = [{
-        "title": "Centroid Decomposition Explained",
-        "excerpt":"So in this post I would like to explain a beautiful divide and conquer technique used on trees called Centroid Decomposition with help of few example problems. This post is inspired by this original post by Tanuj Khattar. Please read that post first for the theory part of the centroid...","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/centroid-decomposition-explained/",
-        "teaser":null}]
